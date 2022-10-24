8f3a611e045981de8b191f62788210bf9c4a7af1
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-10-24 21:37:54 +0200

    job 2

---
+++ job02.py
@@ -0,0 +1,12 @@
+"""
+mohamed, le 17 oct 2022
+Créez un script job02.py qui dans un premier temps va afficher “Bonjour, comment
+t’appelles tu ? ”
+L’utilisateur devra ensuite entrer son prénom.
+Déclarez une variable prenom, qui prendra la valeur saisie par l’utilisateur.
+A l’aide d’une fonction system, “Bonjour prenom” (prenom étant la valeur de la variable
+du même nom)
+"""
+print("Bonjour, comment t’appelles tu?")
+prenom = input()
+print("bonjour", prenom)
