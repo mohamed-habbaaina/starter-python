34bca2463f4bcdb1270a59db273853c99e5a456a
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-10-24 21:43:31 +0200

    job 6 Les boucles infinies

---
+++ job06.py
@@ -0,0 +1,13 @@
+"""
+Mohamed HABBAAINA, le 17 oct 2022
+Créez un script job06.py
+Lorsque l’utilisateur va lancer le script, un prompteur devra s’afficher “>”, et le
+programme devra attendre un input.
+Si l’input entré est “Bonjour”, le programme devra répondre “Bonjour à toi”
+Si l’input entré est “Au revoir”, le programme devra quitter
+"""
+x = input(">")
+if x == "bonjour":
+    print("bonjour a toi")
+elif x == "Au revoir":
+    exit()
