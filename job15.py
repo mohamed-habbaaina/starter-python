433749b43c7e129b74a8c7b9e096318e9b7bbe18
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-10-29 11:09:15 +0200

    La fonction “Bonjour prenom nom”)

---
+++ job15.py
@@ -0,0 +1,9 @@
+"""
+Mohamed HABBAAINA, le 19 oct 2022
+Créer un programme job15.py. Le programme devra contenir une fonction qui prend en
+paramètres une variable prenom et une variable nom.
+La fonction écrira “Bonjour prenom nom” dans le terminal.
+"""
+def MyFonction(prenom, nom):
+    print("Bonjour", prenom, nom)
+MyFonction("Mohamed","HABBAAINA")
