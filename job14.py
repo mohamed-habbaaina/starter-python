ac145e30a24b2cab9d83d0a54a3c2bd72ba2b18b
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-10-29 11:07:51 +0200

    La fonction myFunction()

---
+++ job14.py
@@ -0,0 +1,8 @@
+"""
+Mohamed HABBAAINA, le 19 oct 2022
+Créer un programme job14.py. Le programme devra contenir une fonction myFunction()
+qui écrit dans le terminal “je suis une fonction”.
+"""
+def MyFonction():
+    print("Je suis une fonction ")
+MyFonction()
