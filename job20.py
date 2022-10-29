3e2c555e44a269e171146fdad070ac5c734557aa
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-10-29 11:21:50 +0200

    La fonction myAddition

---
+++ job20.py
@@ -0,0 +1,11 @@
+"""
+Mohamed HABBAAINA, le 19 oct 2022
+Créer un programme job20.py. Le programme devra contenir une seule fonction
+nommée myAddition qui prend en paramètres deux nombres et qui retourne le résultat
+de leur addition.
+Vous devrez appeler cette fonction et assigner à une variable le résultat qu’elle retourne.
+"""
+def myAddition(x, y):
+    resultat= x + y
+    print(resultat)
+myAddition(4,8)
